8f750a5b66afc14229fa61d2958253529d750672
Volker Lorrmann <z0043ddz@macvgfd7qj3n6.speedport.ip>
2023-10-09 19:20:09 +0200

    debug scan()

--- pydala/dataset.py
+++ pydala/dataset.py
@@ -592,7 +592,10 @@ class ParquetDataset(ParquetDatasetMetadata):
                 # chech if fe is a date string
                 matched_string = re.findall("(\d{4}-\d{1,2}-\d{1,2})", fe)
                 print(matched_string)
+
                 if len(matched_string):
+                    print(matched_string[0])
+                    print(fe)
                     is_date = matched_string[0] == fe
                 else:
                     is_date = False
