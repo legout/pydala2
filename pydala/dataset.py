1a09f8160bd80505df4b8ce4a2fe7c523941c964
Volker Lorrmann <z0043ddz@macvgfd7qj3n6.speedport.ip>
2023-12-11 21:44:26 +0100

    fix: timestamp_column is None as default in with_datepart_columns

--- pydala/dataset.py
+++ pydala/dataset.py
@@ -889,6 +889,7 @@ class ParquetDataset(ParquetDatasetMetadata):
         writer.sort_data(by=sort_by)
         writer.unique(columns=unique)
         writer.add_datepart_columns(timestamp_column=self._timestamp_column)
+        print("timestamp_column, write_to_dataset", self._timestamp_column)
         writer.cast_schema(
             use_large_string=use_large_string,
             ts_unit=ts_unit,
