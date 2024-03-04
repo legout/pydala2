77e7f084c98e734664e309157195c5751d0acee5
Volker Lorrmann <z0043ddz@macvgfd7qj3n6.speedport.ip>
2024-03-04 19:11:33 +0100

    fix: copy metadata to metadata_temp in update_metadata

--- pydala/metadata.py
+++ pydala/metadata.py
@@ -170,10 +170,10 @@ class ParquetDatasetMetadata:
 
         if files is not None:
             new_files = sorted(set(files + new_files))
-
+        print(new_files)
         if new_files:
             self._collect_file_metadata(files=new_files, **kwargs)
-
+        print(rm_files)
         if rm_files:
             self._rm_file_metadata(files=rm_files)
 
@@ -538,7 +538,7 @@ class ParquetDatasetMetadata:
             A list of file paths referenced in the metadata of the dataset.
         """
         if self.has_metadata:
-            return list(
+            return sorted(
                 set(
                     [
                         self._metadata.row_group(i).column(0).file_path.lstrip("../")
