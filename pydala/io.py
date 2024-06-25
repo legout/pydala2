41a5d305caf3a12dbf120e1419ba7afaf7550d0b
Volker Lorrmann <z0043ddz@macvgfd7qj3n6.speedport.ip>
2024-06-25 19:58:16 +0200

    added  to with_datepart_columns

--- pydala/io.py
+++ pydala/io.py
@@ -7,6 +7,7 @@ import pandas as pd
 import polars.selectors as cs
 import pyarrow as pa
 import pyarrow.dataset as pds
+
 # import pyarrow.dataset as pds
 import pyarrow.parquet as pq
 from fsspec import AbstractFileSystem
@@ -14,7 +15,7 @@ from fsspec import filesystem as fsspec_filesystem
 
 from .helpers.datetime import get_timestamp_column
 from .helpers.misc import get_partitions_from_path
-from .helpers.polars_ext import pl
+from .helpers.polars import pl
 from .schema import convert_timestamp, replace_schema, shrink_large_string
 from .table import PydalaTable
 
@@ -263,7 +264,8 @@ class Writer:
             datepart_columns = {
                 col: True
                 for col in self.schema.names + columns
-                if col in ["year", "month", "week", "yearday", "monthday", "weekday"]
+                if col
+                in ["year", "month", "week", "yearday", "monthday", "weekday", "day"]
             }
 
             self.data = self.data.with_datepart_columns(
